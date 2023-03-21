bb8d85950b8847cb07f60cbd595166a24506284c
KhuongNC <anhkhuongmurom@gmail.com>
2023-03-21 23:26:51 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Weather />
+</template>
+
+<script>
+import Weather from "./components/WeatherComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    Weather,
+  },
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+
+body {
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
